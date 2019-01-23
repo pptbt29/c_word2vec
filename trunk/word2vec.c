1f012866369cffb19dbe65bbdac3802a8cd72635
pptbt29 <pptbt29@gmail.com>
2019-01-23 13:10:01 +0800

    set the max size of vocab to 60900000 (87000000*0.7)

--- trunk/word2vec.c
+++ trunk/word2vec.c
@@ -24,7 +24,7 @@
 #define MAX_SENTENCE_LENGTH 1000
 #define MAX_CODE_LENGTH 40
 
-const int vocab_hash_size = 30000000;  // Maximum 30 * 0.7 = 21M words in the vocabulary
+const int vocab_hash_size = 87000000;  // Maximum 30 * 0.7 = 21M words in the vocabulary
 
 typedef float real;                    // Precision of float numbers
 
